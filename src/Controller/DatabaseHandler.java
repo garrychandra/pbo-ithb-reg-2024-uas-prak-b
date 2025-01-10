4d89d755f5b7934a9b72cac16a72a1d2968a8c89
garrychandra <garryalexanderchandra97@gmail.com>
2025-01-10 09:14:57 +0700

    init commit

---
+++ src/Controller/DatabaseHandler.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class DatabaseHandler extends SQLException{
+
+    public static Connection con;
+    private static String driver = "com.mysql.cj.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost/tokosidia_db";
+    private static String username = "root";
+    private static String password = "";
+
+    private static Connection logOn() {
+        try {
+            Class.forName(driver).newInstance();
+            con = DriverManager.getConnection(url, username, password);
+        } catch (Exception ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getLocalizedMessage());
+            JOptionPane.showMessageDialog(null, "Error Ocurred when login" + ex);
+        }
+        return con;
+    }
+    private static void logOff() {
+        try {
+            // tutup koneksi
+            con.close();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error Ocurred when login" + ex);
+        }
+    }
+
+    public static Connection connect() {
+        try {
+            con = logOn();
+        } catch (Exception ex) {
+            System.out.println("Error occured when connecting to database");
+        }
+        return con;
+    }
+
+    public static void disconnect() {
+        try {
+            logOff();
+        } catch (Exception ex) {
+            System.out.println("Error occured when connecting to database");
+        }
+    }
+}
